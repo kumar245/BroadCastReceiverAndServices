3458ac6278de83e7faa8c1c9d9ae8d26f96c495c
chaitu <popurichaitanyakumar@gmail.com>
2017-05-26 17:33:09 -0500

    Initial commit

---
+++ app/src/main/java/com/kumar/user/broadcastreceiverandservices/MainActivity.java
@@ -0,0 +1,56 @@
+package com.kumar.user.broadcastreceiverandservices;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CheckPermissions();
+    }
+
+    public void buSendBroadCast(View view) {
+        Intent intent=new Intent();
+        intent.setAction("com.example.BroadCast");
+        intent.putExtra("msg","hello from activity");
+        sendBroadcast(intent);
+    }
+    void CheckPermissions(){
+        if (Build.VERSION.SDK_INT>=23){
+            if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)!=
+                    PackageManager.PERMISSION_GRANTED) && (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS)!=
+                    PackageManager.PERMISSION_GRANTED) ){
+                requestPermissions(new String[]{
+                        Manifest.permission.RECEIVE_SMS},REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
+    }
+    final private int REQUEST_CODE_ASK_PERMISSIONS=123;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                }else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ app/src/main/java/com/kumar/user/broadcastreceiverandservices/myReceiver.java
@@ -0,0 +1,46 @@
+package com.kumar.user.broadcastreceiverandservices;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by User on 5/26/2017.
+ */
+
+public class myReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle bundle=intent.getExtras();
+        if (intent.getAction().equalsIgnoreCase("com.example.BroadCast")){
+            String msg=bundle.getString("msg");
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+        }
+        if (intent.getAction().equalsIgnoreCase("android.provider.Telephony.SMS_RECEIVED")){
+            if (bundle!=null){
+                final Object[] pdusObj= (Object[]) bundle.get("pdus");
+                SmsMessage[] messages=new SmsMessage[pdusObj.length];
+                for (int i=0;i<messages.length;i++){
+                    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                        String format=bundle.getString("format");
+                        messages[i] =SmsMessage.createFromPdu((byte[]) pdusObj[i],format);
+                    }
+                    else {
+                        messages[i]=SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    }
+                    String senderNum=messages[i].getOriginatingAddress();
+                    String message=messages[i].getMessageBody();
+                    Toast.makeText(context,senderNum + ":" + message, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+            Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
